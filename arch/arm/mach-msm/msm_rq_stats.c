07616476e217d74f21a010261f5fe3ee2f46aed6
Archana Sathyakumar <asathyak@codeaurora.org>
2013-02-20 10:35:35 -0700

    msm:rq_stats: Add hotplug disable attribute
    Register for suspend and resume notifications from kernel. This should
    be done as part of core_initcall so as to receive notification as close
    as possible to the actual event. Add hotplug_disable attribute to
    rq-stats node. Set/clear this attribute based on the suspend/resume events.
    
    CRS-fixed: 452456
    Change-Id: I76fee349b57f47df40785e9c2503e2e1cc6e064e
    Signed-off-by: Archana Sathyakumar <asathyak@codeaurora.org>
    
    Conflicts:
    	arch/arm/mach-msm/msm_rq_stats.c

--- arch/arm/mach-msm/msm_rq_stats.c
+++ arch/arm/mach-msm/msm_rq_stats.c
@@ -206,9 +206,7 @@ static int system_suspend_handler(struct notifier_block *nb,
 	switch (val) {
 	case PM_POST_HIBERNATION:
 	case PM_POST_SUSPEND:
-	case PM_POST_RESTORE:
 		rq_info.hotplug_disabled = 0;
-		break;
 	case PM_HIBERNATION_PREPARE:
 	case PM_SUSPEND_PREPARE:
 		rq_info.hotplug_disabled = 1;
@@ -219,22 +217,6 @@ static int system_suspend_handler(struct notifier_block *nb,
 	return NOTIFY_OK;
 }
 
-static int freq_policy_handler(struct notifier_block *nb,
-			unsigned long event, void *data)
-{
-	struct cpufreq_policy *policy = data;
-	struct cpu_load_data *this_cpu = &per_cpu(cpuload, policy->cpu);
-
-	if (event != CPUFREQ_NOTIFY)
-		goto out;
-
-	this_cpu->policy_max = policy->max;
-
-	pr_debug("Policy max changed from %u to %u, event %lu\n",
-			this_cpu->policy_max, policy->max, event);
-out:
-	return NOTIFY_DONE;
-}
 
 static ssize_t hotplug_disable_show(struct kobject *kobj,
 		struct kobj_attribute *attr, char *buf)
